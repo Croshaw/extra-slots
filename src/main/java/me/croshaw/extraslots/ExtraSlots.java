0d8280694979e37fe5e49a351dbb465f44b7fcd8
Croshaw <crosafixx200215@gmail.com>
2023-08-12 13:07:17 +0500

    init commit

---
+++ src/main/java/me/croshaw/extraslots/ExtraSlots.java
@@ -0,0 +1,10 @@
+package me.croshaw.extraslots;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ExtraSlots implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
---
+++ src/main/java/me/croshaw/extraslots/client/ExtraSlotsClient.java
@@ -0,0 +1,10 @@
+package me.croshaw.extraslots.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class ExtraSlotsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
